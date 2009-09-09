df535c72d73502e74b72f4b4f1bbd6b1b7b629dc
Dobrica Pavlinusic <dpavlin@rot13.org>
2009-09-09 11:42:22 +0000

    fake objectGUID

--- sql/organizationalunit.sql
+++ sql/organizationalunit.sql
@@ -3,6 +3,8 @@ select
 	concat('ou=',c.categorycode,',dc=ffzg,dc=hr')	as dn,
 	'organizationalunit'		as objectClass,
 	c.categorycode			as ou,
-	c.description			as description
+	c.description			as description,
+	-- fake for SafeQ, we don't have numeric primary key
+	crc32(categorycode) % 1000	as objectGUID
 from categories c
 
