e1463955ab5dbb059c645b9eee33d769371e9882
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-02-21 01:03:01 +0000

    push logs into log/ directory

--- koha-ldap-rewrite.sh
+++ koha-ldap-rewrite.sh
@@ -1,4 +1,4 @@
 cd /srv/virtual-ldap/
-log=koha-ldap-rewrite.log 
+log=log/koha-ldap-rewrite.log 
 test -f $log && mv $log $log.`date +%Y-%m-%dT%H%M%S`
 ( ./bin/ldap-rewrite.pl 2>&1 ) | tee -a $log
--- safeq-ldap-koha.sh
+++ safeq-ldap-koha.sh
@@ -1,4 +1,4 @@
 cd /srv/virtual-ldap/
-log=safeq-ldap-koha.log 
+log=log/safeq-ldap-koha.log 
 test -f $log && mv $log $log.`date +%Y-%m-%dT%H%M%S`
 ( MAX_RESULTS=10 ./bin/ldap-koha.pl 10.60.0.13:2389 2>&1 ) | tee -a $log
