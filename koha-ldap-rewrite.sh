f19f31c7b13856a33b305842d2c9ab07e57c620c
Dobrica Pavlinusic <dpavlin@rot13.org>
2015-08-29 15:29:56 +0200

    emit log output to STDOUT

--- koha-ldap-rewrite.sh
+++ koha-ldap-rewrite.sh
@@ -1,4 +1,5 @@
 cd /srv/virtual-ldap/
-log=log/koha-ldap-rewrite.log 
-test -f $log && mv $log $log.`date +%Y-%m-%dT%H%M%S`
-( ./bin/ldap-rewrite.pl 2>&1 ) | tee -a $log
+#log=log/koha-ldap-rewrite.log
+#test -f $log && mv $log $log.`date +%Y-%m-%dT%H%M%S`
+#( ./bin/ldap-rewrite.pl 2>&1 ) | tee -a $log
+./bin/ldap-rewrite.pl
